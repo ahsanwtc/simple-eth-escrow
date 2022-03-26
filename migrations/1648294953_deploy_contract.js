1be4c940b7021ba00cdbdbb85c7049ec82422073
jsan <ahsan.wtc@gmail.com>
2022-03-26 19:10:14 +0500

    passing all tests

--- migrations/1648294953_deploy_contract.js
+++ migrations/1648294953_deploy_contract.js
@@ -1,8 +1,5 @@
 const Escrow = artifacts.require('Escrow');
 
-module.exports = async function(deployer) {
-  const accounts = await web3.eth.getAccounts();
-  const [_, payer, payee] = accounts;
-  const amount = 1000;
-  deployer.deploy(Escrow, payer, payee, amount);
+module.exports = function(deployer, _network, accounts) {
+  deployer.deploy(Escrow, accounts[1], accounts[2], 1000);
 };
--- test/Escrow.test.js
+++ test/Escrow.test.js
@@ -10,9 +10,10 @@ const asserError = async (promise, error) => {
   try {
     await promise;
   } catch (e) {
-    assert(e.message.includes(error));
+    assert(e.reason === error);
+    return;
   }
-  assert(false);
+  assert(false, 'should not reach here');
 };
 
 contract('Escrow', accounts => {
@@ -23,5 +24,42 @@ contract('Escrow', accounts => {
     escrow = await Escrow.deployed();
   });
 
+  it('should deposit', async () => {
+    await escrow.deposit({ from: payer, value: 900 });
+    const balance = parseInt(await web3.eth.getBalance(escrow.address));
+    assert(balance === 900, 'wrong balance');
+  });
+
+  it('should NOT deposit if sender is not payer', async () => {
+    await asserError(escrow.deposit({ from: accounts[3], value: 100 }), 'sender must be the payer');
+  });
+
+  it('should NOT deposit if funds exceed amount', async () => {
+    await asserError(escrow.deposit({ from: payer, value: 600 }), 'funds must not exceed the agreed amount');
+  });
+
+  it('should NOT release funds if full amount has not been received', async () => {
+    await asserError(escrow.release({ from: lawyer }), 'cannot release funds before the full amount is paid');
+  });
+
+  it('should NOT release funds if called is not lawyer', async () => {
+    /**
+     * deposit the remaing 100
+     */
+    await escrow.deposit({ from: payer, value: 100 });
+    await asserError(escrow.release({ from: payee }), 'only lawyer can release funds');
+  });
+
+  it('should release funds', async () => {
+    const balanceBefore = web3.utils.toBN(await web3.eth.getBalance(payee));
+    await escrow.release({ from: lawyer });    
+    const balanceAfter = web3.utils.toBN(await web3.eth.getBalance(payee));
+    
+    const balance = balanceAfter.sub(balanceBefore);
+    assert(balance.toNumber() === 1000);
+    const constratBalance = await web3.eth.getBalance(escrow.address);
+    assert(parseInt(constratBalance) === 0);
+  });
   
 });
+  
